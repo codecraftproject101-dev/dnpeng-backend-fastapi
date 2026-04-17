d33e5b21d6f2991826395d39d61be66b7e43ab6c
twentiecker <twentiecker@gmail.com>
2026-04-17 10:00:56 +0700

    update dengan feature login dan perbaikan konversi

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel, EmailStr
+from enum import Enum
+
+
+class UserRole(str, Enum):
+    pkrt = "pkrt"
+    pklnprt = "pklnprt"
+    pkp = "pkp"
+    pmtb = "pmtb"
+    pi = "pi"
+    xm = "xm"
+    konsolidator = "konsolidator"
+    direktur = "direktur"
 
 
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
+    role: UserRole  # 🔥 wajib & tervalidasi otomatis
 
 
 class UserResponse(BaseModel):
